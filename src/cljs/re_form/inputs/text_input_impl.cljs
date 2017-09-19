765565d445ec673a8698f3b265f1b838569e580e
Aitem <muradbei@gmail.com>
2017-09-19 09:34:59 +0300

    dissoc attr

--- src/cljs/re_form/inputs/text_input_impl.cljs
+++ src/cljs/re_form/inputs/text_input_impl.cljs
@@ -4,5 +4,7 @@
 (defn text-input [props]
   (let [my-onchange (fn [event on-change] (on-change (.. event -target -value)))]
     (fn [{:keys [value on-change] :as props}]
-      [:input (merge {:type "text"} props {:on-change #(my-onchange % on-change)
-                                           :value value})])))
+      [:input (merge {:type "text"}
+                     (dissoc props :form-name)
+                     {:on-change #(my-onchange % on-change)
+                      :value value})])))
