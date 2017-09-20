e4dc0ac18d3c9bad55a54ce24b12d9a5ecca2181
Evgeny Fomin <fominok@hotmail.com>
2017-09-21 02:53:14 +0300

    semi-working select-xhr

--- src/cljs/re_form/inputs/select_xhr_impl.cljs
+++ src/cljs/re_form/inputs/select_xhr_impl.cljs
@@ -45,39 +45,47 @@
      [:&:hover {:background-color "#f1f1f1"}]]]])
 
 (defn- suggest-set-callback [state suggs]
-  (println suggs)
   (swap! state assoc :suggestions suggs))
 
-(defn- on-change-update [state suggest-fn query]
-  (go (suggest-set-callback state (<! (suggest-fn query)))))
+(defn- on-change-update [state suggest-fn node]
+  (let [query (.-textContent node)]
+    (when-not (empty? query)
+      (go (suggest-set-callback state (<! (suggest-fn query)))))))
 
 ;; TODO:
 ;; text input to query
 ;; on component-did-mount do r/dom-node and .addEventListener 'input' to track
 ;; contenteditable changes
 ;; debounce
-(defn select-xhr-input [_]
+(defn select-xhr-input [{:keys [suggest-fn]}]
   (let [state (r/atom {:active false :suggestions []})]
-    (fn [{:keys [value on-change value-fn label-fn match-fn suggest-fn placeholder]}]
-      (let [label-fn (or label-fn pr-str)
-            value-fn (or value-fn identity)
-            match-fn (or match-fn
-                         (fn [v] (->> (:suggestions @state)
-                                      (filter #(= (value-fn %) v))
-                                      first label-fn)))]
-        [:div.re-select-xhr
-         {:on-click #(swap! state update :active not)}
-         (if value
-           [:span.value
-            [:span.value (match-fn value) ]]
-           [:span.choose-value
-            {:contentEditable true
-             :on-input #(on-change-update state suggest-fn (.. % -target -value))}])
-         (when (:active @state)
-           [:div.options
-            (for [i (:suggestions @state)] ^{:key (label-fn i)}
-              [:div.option
-               {:on-click (fn [_] (on-change (value-fn i)))
-                :class (when (= value (value-fn i)) "active")}
-               (label-fn i)])])])))
+    (r/create-class
+     {:component-did-mount
+      (fn [this]
+        (let [root-node (r/dom-node this)
+              node (first (array-seq (.getElementsByClassName root-node "choose-value")))]
+          (.addEventListener node "input" #(on-change-update state suggest-fn node))))
+
+      :reagent-render
+      (fn [{:keys [value on-change value-fn label-fn match-fn suggest-fn placeholder]}]
+        (let [label-fn (or label-fn pr-str)
+              value-fn (or value-fn identity)
+              match-fn (or match-fn
+                           (fn [v] (->> (:suggestions @state)
+                                        (filter #(= (value-fn %) v))
+                                        first label-fn)))]
+          [:div.re-select-xhr
+           {:on-click #(swap! state update :active not)}
+           (if value
+             [:span.value
+              [:span.value (match-fn value) ]]
+             [:span.choose-value
+              {:contentEditable true}])
+           (when (:active @state)
+             [:div.options
+              (for [i (:suggestions @state)] ^{:key (label-fn i)}
+                [:div.option
+                 {:on-click (fn [_] (on-change (value-fn i)))
+                  :class (when (= value (value-fn i)) "active")}
+                 (label-fn i)])])]))}))
   )
