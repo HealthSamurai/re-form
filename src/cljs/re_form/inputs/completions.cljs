b8d9ce5b77c0bd86da531afff9172f36ba0730f9
Evgeny Fomin <fominok@hotmail.com>
2017-10-09 17:00:16 +0300

    add complete-incremental

--- src/cljs/re_form/inputs/completions.cljs
+++ src/cljs/re_form/inputs/completions.cljs
@@ -10,7 +10,7 @@
           cur-pos
           (recur cur-pos))))))
 
-(defn- options [dict regex word]
+(defn- options-sw [dict regex word]
   (dict (clojure.string/replace word regex "")))
 
 (defn complete-startswith [regex dict cm option]
@@ -23,9 +23,32 @@
         to {:line (.-line cursor) :ch end-char}
         word (.getRange cm (clj->js from) (clj->js to))]
     (when start-char
-      (when-let [lst (options dict regex word)]
+      (when-let [lst (options-sw dict regex word)]
         (clj->js
          {:list lst
           :from from
           :to to})))))
 
+(defn- options-incr [words value-fn regex word]
+  (let [cropped-word (clojure.string/replace word regex "")
+        extract-fn (if (map? (first words)) :text identity)]
+    (filter #(-> %
+                 extract-fn
+                 value-fn
+                 (clojure.string/starts-with? cropped-word)) words)))
+
+(defn complete-incremental [regex words value-fn cm option]
+  (let [cursor (.getCursor cm)
+        end-char (.-ch cursor)
+        line (.getLine cm (.-line cursor))
+        start-char (search-backwards line end-char regex)
+        to-pos #(.Pos js/CodeMirror line %)
+        from {:line (.-line cursor) :ch start-char}
+        to {:line (.-line cursor) :ch end-char}
+        word (.getRange cm (clj->js from) (clj->js to))]
+    (when start-char
+      (when-let [lst (options-incr words value-fn regex word)]
+        (clj->js
+         {:list lst
+          :from from
+          :to to})))))
