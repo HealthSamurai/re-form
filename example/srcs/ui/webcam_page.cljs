f0f98d3a30c56f4ace97820b92fbc186a38a6bb0
Evgeny Fomin <fominok@hotmail.com>
2018-01-25 16:53:47 +0300

    webcam upload

--- example/srcs/ui/webcam_page.cljs
+++ example/srcs/ui/webcam_page.cljs
@@ -1,12 +1,20 @@
 (ns ui.webcam-page
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [ui.routes :as ui-routes]
             [re-frame.core :as rf]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
             [re-form.inputs :as inputs]))
 
 (rf/reg-sub
  ::thumbs
  (fn [db]
    (::thumbs db)))
+
+(rf/reg-sub
+ ::pics
+ (fn [db]
+   (::pics db)))
 
 (rf/reg-event-db
  ::init
@@ -39,6 +47,15 @@
   (rf/dispatch [::init])
   (fn []
     [:div
+     [:button.btn.btn-primary
+      {:on-click
+       (fn [_]
+         (go (doall (for [f @(rf/subscribe [::pics])]
+                      (http/post
+                       "http://localhost:8080/$upload"
+                       {:multipart-params
+                        {:file f}})))))}
+      "Upload"]
      [inputs/thumbs {:thumbs (rf/subscribe [::thumbs])
                      :remove-fn #(rf/dispatch [::remove %])}]
      [inputs/webcam {:width 640
--- src/cljs/re_form/inputs/webcam_impl.cljs
+++ src/cljs/re_form/inputs/webcam_impl.cljs
@@ -5,30 +5,39 @@
 ;; A widget to take webcam photos
 ;; `new-thumb` of `webcam` component is a function to process base64 thumbnails
 ;; (`divisor` times smaller by each axis)
-;; `new-pic` of `webcam` component is a function to process base64 images (fullsize)
+;; `new-pic` of `webcam` component is a function to process blob images (fullsize)
 ;;
-;; In `ui.webcam-page` example handlers are re-frame db events to store base64 in db
+;; In `ui.webcam-page` example handlers are re-frame db events
+;; to store base64 and blobs in db
 ;;
-;; Using subscription `thumbs` component can show the images gallery.
+;; Using subscription `thumbs` component can show the images gallery (base64 thumbnails).
 
 (defn webcam-style
   [{:keys [h h2 h3 selection-bg-color hover-bg-color border]}]
-  [:.thumbs
-   {:display :flex
-    :flex-wrap :wrap}
-   [:.thumbnail
-    {:display :inline-block
-     :margin (u/px 10)
-     :position :relative}
-    [:.cross {:display :none}]
-    [:&:hover
-     [:.cross
-      {:position :absolute
-       :top 0
-       :display :inline-block
-       :cursor :pointer
-       :color hover-bg-color
-       :right (u/px 5)}]]]])
+  [:*
+   [:.re-webcam
+    {:width (u/px 640)}
+    [:.controls
+     {:display :flex
+      :justify-content :center
+      :margin (u/px 10)}
+     [:button {:margin (u/px 10)}]]]
+   [:.thumbs
+    {:display :flex
+     :flex-wrap :wrap}
+    [:.thumbnail
+     {:display :inline-block
+      :margin (u/px 10)
+      :position :relative}
+     [:.cross {:display :none}]
+     [:&:hover
+      [:.cross
+       {:position :absolute
+        :top 0
+        :display :inline-block
+        :cursor :pointer
+        :color hover-bg-color
+        :right (u/px 5)}]]]]])
 
 (defn- thumbnail [data remove-fn]
   [:div.thumbnail
@@ -47,6 +56,19 @@
 
 (defn- hide [& nodes] (apply-display nodes "none"))
 (defn- show [& nodes] (apply-display nodes "block"))
+
+#_(defn- to-blob [data-uri]
+    (let [splitted (.split data-uri ",")
+          byte-string-conv (if (>= (-> (.split data-uri ",")
+                                       (aget 0)
+                                       (.indexOf "base64")) 0)
+                             js/atob js/unescape)
+          byte-string (byte-string-conv (aget splitted 1))
+          mime-string (-> splitted (aget 0) (.split ":") (aget 1) (.split ";") (aget 0))
+          ia (js/Uint8Array. (.-length byte-string))]
+      (for [i (range (.-length byte-string))]
+        (aset ia i (.charCodeAt byte-string i)))
+      (js/Blob. #js [ia] (clj->js {:type mime-string}))))
 
 (defn webcam [{:keys [width height divisor new-thumb new-pic] :or {divisor 10}}]
   (let [state (r/atom {})]
@@ -69,10 +91,11 @@
                                                 -URL
                                                 (createObjectURL stream)))
                         (.play video)))))
-          (.addEventListener snap "click" (fn []
-                                              (.drawImage context video 0 0 width height)
-                                              (show canvas accept discard)
-                                              (hide video snap)))
+          (.addEventListener snap "click"
+                             (fn []
+                               (.drawImage context video 0 0 width height)
+                               (show canvas accept discard)
+                               (hide video snap)))
           (.addEventListener discard "click"
                              (fn []
                                (hide canvas accept discard)
@@ -87,24 +110,26 @@
                                  (set! (.-height small-canvas) h)
                                  (.drawImage ctx canvas 0 0 w h)
                                  (new-thumb (.toDataURL small-canvas "image/png"))
-                                 (new-pic (.toDataURL canvas "image/png"))
+                                 (.toBlob canvas new-pic)
                                  (hide canvas accept discard)
                                  (show video snap))))))
 
       :reagent-render
       (fn [{:keys [upload-fn uploading]}]
         [:div.re-webcam
-         [:button {:ref #(swap! state assoc :snap %)
-                   :style {:display :block}} "Take a shot"]
-         [:button {:ref #(swap! state assoc :accept %)
-                   :style {:display :none}} "Ok"]
-         [:button {:ref #(swap! state assoc :discard %)
-                   :style {:display :none}} "Discard"]
-         [:video {:ref #(swap! state assoc :video %)
-                  :width width
-                  :height height
-                  :autoPlay true}]
-         [:canvas {:ref #(swap! state assoc :canvas %)
-                   :style {:display :none}
+         [:div.controls
+          [:button.btn.btn-primary {:ref #(swap! state assoc :snap %)
+                                    :style {:display :block}} "Take a shot"]
+          [:button.btn.btn-success {:ref #(swap! state assoc :accept %)
+                                    :style {:display :none}} "Ok"]
+          [:button.btn {:ref #(swap! state assoc :discard %)
+                        :style {:display :none}} "Discard"]]
+         [:div.visuals
+          [:video {:ref #(swap! state assoc :video %)
                    :width width
-                   :height height}]])})))
+                   :height height
+                   :autoPlay true}]
+          [:canvas {:ref #(swap! state assoc :canvas %)
+                    :style {:display :none}
+                    :width width
+                    :height height}]]])})))
